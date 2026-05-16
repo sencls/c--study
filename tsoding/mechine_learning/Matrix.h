afab245babe75f44bde4c6c65ea0a60fbeb083f5
sencls <1158309140@qq.com>
2026-05-16 12:10:39 +0800

    add Matrix

--- tsoding/mechine_learning/Matrix.h
+++ tsoding/mechine_learning/Matrix.h
@@ -3,15 +3,21 @@
 #include <iostream>
 #include <vector>
 #include <cassert>
+#include <cmath>
 float rand_float()
 {
     return (float)rand() / (float)RAND_MAX;
 }
-
+float sigmoidf(float x)
+{
+    return 1.f / (1.f + exp(-x));
+}
 class Matrix
 {
 public:
-    Matrix(size_t rows, size_t cols, float value = 0) : _rows(rows), _cols(cols), matrix(rows, std::vector<float>(cols, value)) {}
+#define mat_printn(x) mat_print(x, #x)
+    Matrix(size_t rows, size_t cols, float value = 0) : _rows(rows),
+                                                        _cols(cols), matrix(rows, std::vector<float>(cols, value)) {}
     friend Matrix operator+(const Matrix &a, const Matrix &b)
     {
         assert(b._cols == a._cols && b._rows == a._rows);
@@ -44,8 +50,9 @@ public:
         }
         return t;
     }
-    friend void mat_print(Matrix &other)
+    friend void mat_print(Matrix &other, std::string name)
     {
+        std::cout << name << "=[\n";
         for (const auto &t : other.matrix)
         {
             for (const auto &v : t)
@@ -54,14 +61,31 @@ public:
             }
             std::cout << '\n';
         }
+        std::cout << "]\n";
     }
     friend void mat_rand(Matrix &other, float low, float high)
     {
+
         for (auto &t : other.matrix)
         {
             for (auto &v : t)
             {
                 v = rand_float() * (high - low) + low;
+            }
+        }
+    }
+    std::vector<float> &operator[](int x)
+    {
+        return matrix[x];
+    }
+
+    friend void mat_sig(Matrix &m)
+    {
+        for (int i = 0; i < m._rows; ++i)
+        {
+            for (int j = 0; j < m._cols; ++j)
+            {
+                m[j][j] = sigmoidf(m[i][j]);
             }
         }
     }
--- tsoding/mechine_learning/test.cpp
+++ tsoding/mechine_learning/test.cpp
@@ -3,10 +3,31 @@
 int main()
 {
     srand(time(0));
-    Matrix a(1, 2, 10);
-    Matrix b(2, 2, 4);
-    Matrix rt = a * b;
+    Matrix x(1, 2);
 
-    mat_print(rt);
+    x[0][0] = 0;
+    x[0][1] = 1;
+
+    Matrix w1(2, 2);
+    Matrix b1(1, 2);
+    Matrix a1(1, 2);
+
+    a1 = x * w1 + b1;
+    mat_sig(a1);
+
+    Matrix w2(2, 1);
+    Matrix b2(1, 1);
+
+    mat_rand(w1, 0, 1);
+    mat_rand(w2, 0, 1);
+    mat_rand(b1, 0, 1);
+    mat_rand(b2, 0, 1);
+
+    mat_printn(x);
+    mat_printn(w1);
+    mat_printn(w2);
+    mat_printn(b1);
+    mat_printn(b2);
+
     return 0;
 }
